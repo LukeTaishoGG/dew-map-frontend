e02aece0b581cb738ddde86e1e440c6aba674136
Taisho <luke.wood.et@gmail.com>
2026-01-18 05:33:49 +0900

    ディレクトリ構成作成

---
+++ app/app/components/header/index.tsx
@@ -0,0 +1,9 @@
+import styles from "@/components/header/index.module.css";
+
+export default function Header() {
+    return (
+        <div className={styles.header}>
+            <h1>DewMap</h1>
+        </div>
+    )
+}
